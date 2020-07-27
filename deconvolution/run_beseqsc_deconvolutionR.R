93714caa6438d9a37969c062d0afc356131c5135
Ian Rose <imr923@gmail.com>
2020-07-26 22:19:33 -0400

    cleaned up R scripts

--- deconvolution/run_beseqsc_deconvolutionR.R
+++ deconvolution/run_beseqsc_deconvolutionR.R
@@ -1,21 +1,15 @@
-.libPaths(new='/home/ian/anaconda3/envs/bseqsc_Env/lib/R/library')
-load('/home/ian/fast_files/start_deconvolution_III.rdata')
+.libPaths(new='YOUR_R_PATH')
+load('YOUR_DATA.rdata')
 
 library(bseqsc)
 
-#load B
-#load raw_minimal_set
-#load ruv_minimal_set
-#raw_minimal_set<-load('/home/ian/fast_files/ruv_minimal_set')
-#ruv_minimal_set<-load('/home/ian/fast_files/ruv_minimal_set')
-#B<-load('/home/ian/fast_files/B')
 
 raw_fit<-bseqsc_proportions(raw_minimal_set, B, verbose=TRUE, log=FALSE)
 
 ruv_fit<-bseqsc_proportions(ruv_minimal_set, B, verbose=TRUE, log=FALSE)
 
 
-save.image('/home/ian/fast_files/end_deconvolution_III.rdata')
+save.image('SAVED_R_DATA.rdata')
 # save raw_fit
 #save(raw_fit, file='./raw_fit')
 #save(ruv_fit, file='./ruv_fit')
--- deconvolution/run_singleR.R
+++ deconvolution/run_singleR.R
@@ -1,7 +1,7 @@
 # An R script to run singleR to ID cell types in my single cell data, becasue I think that R is not playing nice with anndata objecty in ipython
 
 #set R location
-.libPaths(new='/home/ian/anaconda3/envs/SC_Env/lib/R/library')
+.libPaths(new='YOUR_R_PATH')
 
 #load libraries
 library(scRNAseq)
@@ -19,36 +19,15 @@ library(MAST)
 library(purrr)
 
 #load R data
-load('/home/ian/fast_files/.r_adata')
-#r_adata<-load('/home/ian/fast_files/fast_r_adata')
-#cluster_df<-load('/home/ian/fast_files/fast_cluster_df')
-#hpca.se<-load('/home/ian/fast_files/fast_hpca.se')
+load('YOUR_DATA')
 
-#I think I need to make a ColData, or similar column for r_adata
-#also, make sure the labels are correct... ->rownames look lige gene symbols in hpca.se
+
 coldata<-data.frame(row.names=colnames(r_adata), as.vector(cluster_df))
-#wait, maybe as.character meant that only the first whatever number of columns wre used...
 
-#try to do the cell type call
-#I need to use cluster method...
-#test_celltype<-SingleR(test=r_adata, ref=hpca.se, labels = hpca.se$label.main)
-#so, this runs but makes an error...
-#test_celltype<-SingleR(test=r_adata, ref=hpca.se, labels = hpca.se$label.main,
-#	method='cluster',
-#	clusters=cluster_df$clusters)
 
 test_celltype<-SingleR(test=r_adata, ref=hpca.se, labels = hpca.se$label.main,
 	method='cluster',
 	clusters=cluster_df$cell_type_subcluster)
 
-#wait, I only need whatever the number of clusters is!
 
-
-
-#this runs!! & produces a result
-#save this image so I can load it back into my notebook
-save.image(file="/home/ian/fast_files/.my_r_adata_celltype", version = NULL, safe = TRUE)
-#save(test_celltype, file='/home/ian/fast_files/fast_test_cell_type')
-
-#load('./.r_adata_celltype')
-#names(test_celltype)
+save.image(file="YOUR_RESULTS", version = NULL, safe = TRUE)
